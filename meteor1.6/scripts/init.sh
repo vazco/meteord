dbb0b9eb76a783d429fdad668c22acdedebe5eb3
Michal Wanat <michalwanat@gmail.com>
2017-10-11 14:16:34 +0200

    optimizations

--- meteor1.6/scripts/init.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-bash $METEORD_DIR/lib/install_imagemagick.sh
-bash $METEORD_DIR/lib/install_phantomjs.sh
--- meteor1.6/scripts/lib/install_imagemagick.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-apt-get update -y
-apt-get install -y imagemagick libmagickcore-dev libmagickwand-dev
