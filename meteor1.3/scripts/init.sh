1f00c9ba349862d0d07eb553e07a219d93e018f9
Michal Wanat <michalwanat@gmail.com>
2017-10-16 12:23:59 +0200

    optimizations

--- meteor1.3/scripts/init.sh
+++ meteor1.3/scripts/init.sh
@@ -2,3 +2,4 @@ set -e
 
 bash $METEORD_DIR/lib/install_imagemagick.sh
 bash $METEORD_DIR/lib/install_phantomjs.sh
+bash $METEORD_DIR/lib/cleanup.sh
---
+++ meteor1.6/scripts/init.sh
@@ -0,0 +1,5 @@
+set -e
+
+bash $METEORD_DIR/lib/install_imagemagick.sh
+bash $METEORD_DIR/lib/install_phantomjs.sh
+bash $METEORD_DIR/lib/cleanup.sh
