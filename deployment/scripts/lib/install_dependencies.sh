bc3a181a68647fb2e8d8a80d1355b94c0b81fa0b
Michal Wanat <michalwanat@gmail.com>
2018-02-16 10:56:37 +0100

    fix for deployment image

--- deployment/scripts/lib/install_dependencies.sh
+++ deployment/scripts/lib/install_dependencies.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-curl https://install.meteor.com/ | sh
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-
-nvm install 8
-nvm use 8
-
 npm install --global npm
 npm install --global mup
 npm install --global uup
 npm install --global pm2-meteor
+
+curl https://install.meteor.com/ | sh
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+
+nvm install 6
+nvm install 7
+nvm install 8
 
 apt-get update -y
