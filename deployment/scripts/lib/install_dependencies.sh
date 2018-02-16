4349e166023a68f22ff269dff9b5b785f9167f76
Michal Wanat <michalwanat@gmail.com>
2018-02-16 11:17:31 +0100

    fix for deployment image

--- deployment/scripts/lib/install_dependencies.sh
+++ deployment/scripts/lib/install_dependencies.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 set -e
 
+npm cache clear --force
+
 npm install --global npm
 npm install --global mup
 npm install --global uup
 npm install --global pm2-meteor
 
+
+echo "Install meteor"
 curl https://install.meteor.com/ | sh
+
+echo "Install nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+echo "Install node 6, 7 and 8"
 nvm install 6
 nvm install 7
 nvm install 8
